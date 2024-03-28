c23c1c6e77e27b6b634632f178bb215c8933caa2
Brilliantbaby <805997907@qq.com>
2024-03-28 19:39:25 +0800

    测试提交

---
+++ LJ_code/java/test1.java
@@ -0,0 +1,9 @@
+package LJ_code.java;
+
+public class test1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println("Hello, World!");
+    }
+}
